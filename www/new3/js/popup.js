27437d3b3586d96a81f2f7327a1001221cbef244
Ann Fadeeva <haineko1512@mail.ru>
2016-01-25 20:30:56 +0300

    начала разбираться с формой загрузки макета

--- www/new3/js/popup.js
+++ www/new3/js/popup.js
@@ -1,8 +1,11 @@
 var offer = document.querySelector(".offer");
+var order = document.querySelector(".order");
 var btn_offer = document.querySelector(".rules--offer");
 var btn_cross = document.querySelector(".offer__cross");
+var orderBtn = document.querySelector(".orderBtn");
+var orderCross = document.querySelector(".order--cross");
 var nav = document.querySelector("nav");
-    
+  
   btn_offer.addEventListener('click', function(event) {
     event.preventDefault();
     offer.classList.add("offer--show");
@@ -13,7 +16,27 @@ var nav = document.querySelector("nav");
     offer.classList.remove("offer--show");
   });
 
+  orderBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    order.classList.add("order--show");
+  }); 
+    
+//  for (var i = 0; i < orderBtn.length; i++) {
+//      orderBtn[i].addEventListener('click', function(event) {
+//        event.preventDefault();
+//        order.classList.add("order--show");
+//      });
+//  }
+
+
+  orderCross.addEventListener('click', function(event) {
+    event.preventDefault();
+    offer.classList.remove("order--show");
+  });
+  
   nav.addEventListener('click', function(event) {
     event.preventDefault();
     offer.classList.remove("offer--show");
   });
+
+  
