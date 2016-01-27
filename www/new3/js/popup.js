9f4d958a8b43682b43c77094cc0c9afa49cfe337
Ann Fadeeva <haineko1512@mail.ru>
2016-01-27 17:38:24 +0300

    добавила размеры для экранов меньше 1400px

--- www/new3/js/popup.js
+++ www/new3/js/popup.js
@@ -1,43 +1,52 @@
 var menu = document.querySelector(".menu");
 var offer = document.querySelector(".offer");
 var order = document.querySelector(".order");
+var sup = document.querySelector(".subscripe");
 var btn_offer = document.querySelector(".rules--offer");
 var btn_cross = document.querySelector(".offer__cross");
 var orderBtn = document.querySelector(".orderBtn");
 var orderCross = document.querySelector(".order--cross");
-var nav = document.querySelector("nav");
-  
-  btn_offer.addEventListener('click', function(event) {
+var btnSup = document.querySelector(".btnSup");
+
+
+btnSup.addEventListener('click', function(event) {
+    console.log("click");
     event.preventDefault();
-    offer.classList.add("offer--show");
-  }); 
-    
-  btn_cross.addEventListener('click', function(event) {
-    event.preventDefault();
-    offer.classList.remove("offer--show");
+    sup.classList.add("show");
+    console.log("add show");
   });
 
-  orderBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    order.classList.add("order--show");
-  }); 
-    
-//  for (var i = 0; i < orderBtn.length; i++) {
-//      orderBtn[i].addEventListener('click', function(event) {
-//        event.preventDefault();
-//        order.classList.add("order--show");
-//      });
-//  }
-
-
-//  orderCross.addEventListener('click', function(event) {
+//  btn_offer.addEventListener('click', function(event) {
 //    event.preventDefault();
-//    offer.classList.remove("order--show");
+//    offer.classList.add("offer--show");
+//  }); 
+//    
+//  btn_cross.addEventListener('click', function(event) {
+//    event.preventDefault();
+//    offer.classList.remove("offer--show");
 //  });
-  
-  nav.addEventListener('click', function(event) {
-    event.preventDefault();
-    offer.classList.remove("offer--show");
-  });
+//
+//  orderBtn.addEventListener('click', function(event) {
+//    event.preventDefault();
+//    order.classList.add("order--show");
+//  }); 
+//    
+////  for (var i = 0; i < orderBtn.length; i++) {
+////      orderBtn[i].addEventListener('click', function(event) {
+////        event.preventDefault();
+////        order.classList.add("order--show");
+////      });
+////  }
+//
+//
+////  orderCross.addEventListener('click', function(event) {
+////    event.preventDefault();
+////    offer.classList.remove("order--show");
+////  });
+//  
+//  nav.addEventListener('click', function(event) {
+//    event.preventDefault();
+//    offer.classList.remove("offer--show");
+//  });
 
   
